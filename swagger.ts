94f1c170f604bb72d0ddb5a049c170ce2f3931f3
Noé <noe.philippe@bob-le-developpeur.com>
2022-11-21 14:05:31 +0100

    Update description

--- swagger.ts
+++ swagger.ts
@@ -28,7 +28,7 @@ export const swaggerDocument = {
     version: '1.0.0',
     title: 'Restauration APIs Document',
     description:
-      "## Token d'authentification : \n ### PROD (https://restauration-api--mds.herokuapp.com) : \n eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNmI4NjdkN2NiNTAxY2QyNGY1MGY0MSIsInVzZXJuYW1lIjoibm9lIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjY5MDE4NTczLCJleHAiOjE2NjkwMjkzNzN9.o4BUPTsUYV2bnFsLpEXIkomy73pWzVBDIXty6MBmPaA \n ### DEV (http://localhost:8888) : \n eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNmI4NjdkN2NiNTAxY2QyNGY1MGY0MSIsInVzZXJuYW1lIjoibm9lIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjY4NTg5OTk5LCJleHAiOjE2Njg2MDA3OTl9.Ru6JcVKxWSaLewgRkOlqh3hp2C5dy-nsKBqwDhpDi5o",
+      "## Token d'authentification : \n ### PROD (https://restauration-api--mds.herokuapp.com) : \n eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNmI4NjdkN2NiNTAxY2QyNGY1MGY0MSIsInVzZXJuYW1lIjoibm9lIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjY5MDM1NjM1LCJleHAiOjE2NjkwNDY0MzV9.9Z4QU9lOHElCkJVOBhUpKzO4s5Rzu6am4MUrX4fF1Wc \n ### DEV (http://localhost:8888) : \n eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNmI4NjdkN2NiNTAxY2QyNGY1MGY0MSIsInVzZXJuYW1lIjoibm9lIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjY4NTg5OTk5LCJleHAiOjE2Njg2MDA3OTl9.Ru6JcVKxWSaLewgRkOlqh3hp2C5dy-nsKBqwDhpDi5o",
     termsOfService: '',
     contact: {
       name: 'Noé PHILIPPE',
